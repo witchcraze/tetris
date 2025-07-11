a71ab7acc4ed990e6cf8c0e81f9e780f5f397fd2
witchcraze <67056980+witchcraze@users.noreply.github.com>
2025-07-11 21:08:01 +0900

    chore: Implement ESLint and code standards (#148)
    This commit introduces ESLint to enforce code quality and consistency across the project.
    
    - package.json:
      - Added ESLint and related TypeScript/Prettier dependencies.
      - Added `lint` and `lint:fix` scripts.
    - .eslintrc.js:
      - Created ESLint configuration with TypeScript and Prettier integration.
    - .eslintignore:
      - Configured ESLint to ignore `node_modules` and `dist` directories.
    - tsconfig.json:
      - Added `lib` option to `compilerOptions` for ESLint to correctly parse browser globals.

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  rules: {
+    // Add custom rules here
+    'prettier/prettier': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
